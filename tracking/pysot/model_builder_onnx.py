a0aee4d6d314f2929031bcdf1a4b16c93aa27761
Keran Kocher <kocher.ke@gmail.com>
2020-12-28 15:48:52 +0100

    use the same path in the ONNX runtime and the Export tool

--- tracking/pysot/model_builder_onnx.py
+++ tracking/pysot/model_builder_onnx.py
@@ -3,32 +3,33 @@ from pysot.core.config import cfg
 from pysot.models.model_builder import ModelBuilder
 
 from tracking.pysot.onnx_adapter import OnnxAdapter
+from tools.export import Export
 
 class ModelBuilderOnnx(ModelBuilder):
-  PATH = "tracking/training/onnx/"
+  MODEL_PATH = Export.OUTPUT_PATH + "onnx/"
 
   def __init__(self):
     super(ModelBuilder, self).__init__()
 
     # build backbone
     # the template method (which initialize the tracking) has different input dimensions
-    self.backbone = OnnxAdapter(self.PATH + "template.onnx")
+    self.backbone = OnnxAdapter(self.MODEL_PATH + "template.onnx")
 
     # build adjust layer
     if cfg.ADJUST.ADJUST:
-        self.neck = OnnxAdapter(self.PATH + "neck.onnx")
+        self.neck = OnnxAdapter(self.MODEL_PATH + "neck.onnx")
 
     # build rpn head
-    self.rpn_head = OnnxAdapter(self.PATH + "rpn_head.onnx")
+    self.rpn_head = OnnxAdapter(self.MODEL_PATH + "rpn_head.onnx")
 
     # build mask head
     if cfg.MASK.MASK:
-        self.mask_head = OnnxAdapter(self.PATH + "mask_head.onnx")
+        self.mask_head = OnnxAdapter(self.MODEL_PATH + "mask_head.onnx")
 
         if cfg.REFINE.REFINE:
-            self.refine_head = OnnxAdapter(self.PATH + "refine_head.onnx")
+            self.refine_head = OnnxAdapter(self.MODEL_PATH + "refine_head.onnx")
 
   def template(self, z):
     super().template(z)
     # Change the backbone because initialization and tracking have differnt input dimensions
-    self.backbone = OnnxAdapter(self.PATH + "backbone.onnx")
+    self.backbone = OnnxAdapter(self.MODEL_PATH + "backbone.onnx")
